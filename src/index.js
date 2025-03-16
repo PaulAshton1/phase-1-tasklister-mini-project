0886fe5f931b8c12eac79a8d8c62e175866d0784
PaulAshton1 <ashtonvilla42@gmail.com>
2025-03-16 19:15:26 +0300

    updated file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+//grabbing the form, input, and ul elements
+const taskform = document.getElementById("create-task-form");
+const newTaskInput = document.getElementById("new-task-description");
+const unorderedTaskList = document.getElementById("tasks");
+const taskpriority = document.querySelector("select");
+const taskDueDate = document.getElementById("due-date");
+
+
+//adding event listener to form
+taskform.addEventListener("submit", (event) => {
+  event.preventDefault(); //prevents form from refreshing the page 
+console.log("Form submitted");
+  
+
+//save a user input as a variable
+  const userText =newTaskInput.value;
+  if(userText === "")
+    return;
+  const userpriority = taskpriority.value;
+  const userDate = taskDueDate.value; 
+
+
+  //create a new list item
+  const newTaskListed = document.createElement("li");
+  newTaskListed.innerText = `${userText.trim} - Due: ${userDueDate.trim || "No Due Date"}`;
+  console.log("Task List Inner HTML", unorderedTaskListTaskListed.innerHTML);
+
+
+  //set color 
+switch(userpriority){
+  case "High":
+    newTaskListed.style.color = "red";
+    break;
+  case "Medium":
+    newTaskListed.style.color = "yellow";
+    break;
+  case "Low":
+    newTaskListed.style.color = "green";
+    break;
+  default:
+    newTaskListed.style.color = "black";
+    break;
+}
+
+
+//appeal to the new list item to unorder list
+unorderedTaskList.appendChild(newTaskListed);
+console.log("UL element found", unorderedTaskList);
+console.log("New task listed", newTaskListed.innerText);
+
+
+//create a delete button
+const deleteButton = document.createElement("button");
+deleteButton.innerText = "delete";
+
+
+//add event listener to the delete button
+deleteButton.addEventListener("click", () => {
+  newTaskListed.removeChild(newTaskListed);
+});
+
+
+//append the delete button to the list item
+newTaskListed.appendChild(deleteButton);
+
+
+//create edit button
+const editButton = document.createElement("button");
+editButton.innerText = "edit";
+
+
+//add event listener to the edit button
+editButton.addEventListener("click", () => {
+  newTaskListed.innerText = prompt("Edit your task", userText);
+  if(newText !== null && newtext !== ""){
+    newTaskListed.firstChild.textContent = `&{newText} - Due: ${userDueDate || "No Due Date"}`;
+  }
+  });
+
+
+  //append the edit button to the list item
+  newTaskListed.appendChild(editButton);
+   taskform.reset();
+});
